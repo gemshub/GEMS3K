5d5859dc2773ac315d72c07dcc3063978fd59868
gems <gems@9ff7d5ee-7a19-0410-ab54-ec97d5369f81>
2010-06-22 11:04:35 +0000

    GEMIPM2K v. 3.0 rev. 458(1658) (in GEMS3 interim release for workshops) DK
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSTANDALONE/trunk@460 9ff7d5ee-7a19-0410-ab54-ec97d5369f81

--- GEMIPM2K/ms_multi_format.cpp
+++ GEMIPM2K/ms_multi_format.cpp
@@ -169,7 +169,7 @@ void TMulti::to_text_file_gemipm( const char *path, bool addMui,
    }
 
 if( _comment )
-{   ff << "# GEMIPM2K v. 3.0 rev. 456(1658)" << endl;
+{   ff << "# GEMIPM2K v. 3.0 rev. 458(1658)" << endl;
    ff << "# Comments can be marked with # $ ; as the first character in the line" << endl << endl;
    ff << "# Template for the ipm-dat text input file for the internal MULTI data" << endl;
    ff << "# (should be read after the DATACH file and before DATABR files)" << endl << endl;
