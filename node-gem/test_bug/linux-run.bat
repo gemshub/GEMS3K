2d78051d23fdf8d52a0418d2e835a250486abb78
kulik <kulik@9ff7d5ee-7a19-0410-ab54-ec97d5369f81>
2012-08-07 16:11:15 +0000

    Deleted obsolete old test subfolders and files from the node-gem example.
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSTANDALONE/trunk@700 9ff7d5ee-7a19-0410-ab54-ec97d5369f81

--- node-gem/test_bug/linux-run.bat
+++
@@ -1,5 +0,0 @@
-cd ..
-exec ./gemnode test_bug/MySystem-dat.lst test_bug/MySystem-dbr-0-0000.dat > test_bug/MySystem.out
-cd test_bug/
-
-
--- node-gem/test_data/linux-run.bat
+++
@@ -1,3 +0,0 @@
-exec ./gemnode test_data/CalcColumn-dat.lst test_data/CalcColumn-dbr-0-0000.dat >  CalcColumn_old12.out  
-
-
