edecc45dd6d1c28df795c2d571e65996983583ff
kulik <kulik@9ff7d5ee-7a19-0410-ab54-ec97d5369f81>
2012-08-14 14:50:10 +0000

    Fixed crash at the end of main.cpp (double call of the destructor).
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSTANDALONE/trunk@703 9ff7d5ee-7a19-0410-ab54-ec97d5369f81

--- node-gem/main.cpp
+++ node-gem/main.cpp
@@ -188,7 +188,7 @@ int main( int argc, char* argv[] )
 
   // deleting GEMIPM and data exchange memory structures
   delete node;
-  mt.~TMyTransport();
+//  mt.~TMyTransport();
   // end of example
   return 0;
 }
