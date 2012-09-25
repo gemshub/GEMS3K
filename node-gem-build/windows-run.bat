5b0b328ba55e26bd38f61653c28111bae942cac1
kulik <kulik@9ff7d5ee-7a19-0410-ab54-ec97d5369f81>
2012-09-25 15:06:51 +0000

    Added folders and files to build and run 'gemcalc' and 'node-gem' examples on Windows.
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSTANDALONE/trunk@713 9ff7d5ee-7a19-0410-ab54-ec97d5369f81

---
+++ node-gem-build/windows-run.bat
@@ -0,0 +1,5 @@
+echo off
+rem release\node-gem.exe tp_test\CalcColumn-dat.lst tp_test\CalcColumn-dbr-0-0000.dat >  CalcColumn_tp.out 
+debug\node-gem.exe tp_test\CalcColumn-dat.lst tp_test\CalcColumn-dbr-0-0000.dat >  CalcColumn_tp.out   
+
+
