cb32d9c64cb7a5d4b07a4857713b7b6898621faf
kulik <kulik@9ff7d5ee-7a19-0410-ab54-ec97d5369f81>
2013-03-27 08:36:05 +0000

    Sync merge from trunk to devDanFit branch.
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSTANDALONE/branches/devDanFit@814 9ff7d5ee-7a19-0410-ab54-ec97d5369f81

--- GEMS3K/ms_multi_format.cpp
+++ GEMS3K/ms_multi_format.cpp
@@ -33,7 +33,7 @@
 #endif
 
 //bool _comment = true;
-const char *_GEMIPM_version_stamp = " GEMS3K v.3.1 r.780 (rc) ";
+const char *_GEMIPM_version_stamp = " GEMS3K v.3.1 r.794 (qwt) ";
 
 //===================================================================
 // in the arrays below, the first field of each structure contains a string
