26dd75d7dd4623f2d132fdb02f2bd36a4b111f7d
kulik <kulik@9ff7d5ee-7a19-0410-ab54-ec97d5369f81>
2014-02-27 10:04:54 +0000

    Changed rev. number to 929 (dev).
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSTANDALONE/branches/devPhase@929 9ff7d5ee-7a19-0410-ab54-ec97d5369f81

--- GEMS3K/ms_multi_format.cpp
+++ GEMS3K/ms_multi_format.cpp
@@ -33,7 +33,7 @@
 #endif
 
 //bool _comment = true;
-const char *_GEMIPM_version_stamp = " GEMS3K v.3.3 r.911 (dev) ";
+const char *_GEMIPM_version_stamp = " GEMS3K v.3.3 r.929 (dev) ";
 
 //===================================================================
 // in the arrays below, the first field of each structure contains a string
