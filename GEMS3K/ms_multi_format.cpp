5031d996101a3c666e7a8f90f50a29142e04e0e3
kulik <kulik@9ff7d5ee-7a19-0410-ab54-ec97d5369f81>
2014-10-10 11:38:03 +0000

    Changed revision number.
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSTANDALONE/branches/devPhase@997 9ff7d5ee-7a19-0410-ab54-ec97d5369f81

--- GEMS3K/ms_multi_format.cpp
+++ GEMS3K/ms_multi_format.cpp
@@ -33,7 +33,7 @@
 #endif
 
 //bool _comment = true;
-const char *_GEMIPM_version_stamp = " GEMS3K v.3.3 r.990 (dev) ";
+const char *_GEMIPM_version_stamp = " GEMS3K v.3.3 r.999 (dev) ";
 
 //===================================================================
 // in the arrays below, the first field of each structure contains a string
