80bf3f1cdc652664141937dc37ac0189d99e582b
kulik <kulik@9ff7d5ee-7a19-0410-ab54-ec97d5369f81>
2015-12-16 09:13:10 +0000

    Changed revision stamp to 1100.
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSTANDALONE/trunk@1100 9ff7d5ee-7a19-0410-ab54-ec97d5369f81

--- GEMS3K/ms_multi_format.cpp
+++ GEMS3K/ms_multi_format.cpp
@@ -33,7 +33,7 @@
 #endif
 
 //bool _comment = true;
-const char *_GEMIPM_version_stamp = " GEMS3K v.3.3 r.1094 (rc) ";
+const char *_GEMIPM_version_stamp = " GEMS3K v.3.3 r.1100 (rc) ";
 
 //===================================================================
 // in the arrays below, the first field of each structure contains a string
