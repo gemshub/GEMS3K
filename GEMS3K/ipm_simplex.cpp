4a6d7593fb924e7c344e93668cdac5f036de0dff
Alina Yapparova <alina.yapparova@gmail.com>
2018-10-26 14:34:39 +0200

    fixed a small compilation error, when compiling GEM-Selektor

--- GEMS3K/ipm_simplex.cpp
+++ GEMS3K/ipm_simplex.cpp
@@ -1090,6 +1090,7 @@ void TMulti::DC_LoadThermodynamicData(TNode* aNa ) // formerly CompG0Load()
 
 #ifndef IPMGEMPLUGIN
   TNode* na;
+  double T;
   if( aNa )
    na = aNa;// for reading GEMIPM files task
   else
