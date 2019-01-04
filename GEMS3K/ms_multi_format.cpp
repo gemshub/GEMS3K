7164a2e4141ea5149c90560ea4bc493a237c5252
Dima Kulik <dmitrii.kulik@psi.ch>
2019-01-04 10:29:29 +0100

    Fixed version and commit numbers for the next bugfix release

--- GEMS3K/ms_multi_format.cpp
+++ GEMS3K/ms_multi_format.cpp
@@ -33,7 +33,7 @@
 #endif
 
 //bool _comment = true;
-const char *_GEMIPM_version_stamp = " GEMS3K v.3.4.4 c.4a6d759 ";
+const char *_GEMIPM_version_stamp = " GEMS3K v.3.4.5 c.39b782e ";
 
 //===================================================================
 // in the arrays below, the first field of each structure contains a string
