c21e940c9e5885f497694b20d6fb7d1e0441a09c
svetad <sv.dmytr@gmail.com>
2025-11-19 18:07:20 +0200

    Move to last thermofun

--- GEMS3K/gems3k_version.h
+++ GEMS3K/gems3k_version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define GEMS3K_VERSION "4.4.3"
-#define GEMS3K_VERSION_HASH "d2367ba"
+#define GEMS3K_VERSION_HASH "33622fe"
 #define GEMS3K_GIT_BRANCH "master"
-#define GEMS3K_ChemicalFun "0.1.8"
-#define GEMS3K_ThermoFun "0.4.5"
+#define GEMS3K_ChemicalFun "0.1.12"
+#define GEMS3K_ThermoFun "0.5.2"
