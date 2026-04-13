319be6c55519cfbf6dcde9af5f3499dd6c9249f4
svetad <sv.dmytr@gmail.com>
2026-04-13 14:09:10 +0300

    Try fix gem2mt build Windows

--- GEMS3K/gems3k_version.h
+++ GEMS3K/gems3k_version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define GEMS3K_VERSION "4.5.2"
-#define GEMS3K_VERSION_HASH "deed64b"
+#define GEMS3K_VERSION_HASH "35a6486"
 #define GEMS3K_GIT_BRANCH "master"
 #define GEMS3K_ChemicalFun "0.1.13"
 #define GEMS3K_ThermoFun "0.5.4"
