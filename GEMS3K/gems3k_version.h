cb6d4789a4dabec77bf2a4f70b43278506da3135
svetad <sv.dmytr@gmail.com>
2026-05-22 15:51:11 +0300

    fixed environment

--- GEMS3K/gems3k_version.h
+++ GEMS3K/gems3k_version.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define GEMS3K_VERSION "4.5.3"
-#define GEMS3K_VERSION_HASH "50d8d0f"
+#define GEMS3K_VERSION "4.5.5"
+#define GEMS3K_VERSION_HASH "2cf017f"
 #define GEMS3K_GIT_BRANCH "master"
 #define GEMS3K_ChemicalFun "0.1.13"
 #define GEMS3K_ThermoFun "0.5.4"
